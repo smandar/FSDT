33c78f0b52028c922b8464ab1fed812f5acac712
Dodo <dodo@ubuntu.ubuntu-domain>
2015-08-08 17:09:46 +0530

    Basic data entry page and vals done

---
+++ Frontend/scripts/googleAutocomplete.js
@@ -0,0 +1,15 @@
+// autocompleting location using google maps api
+
+            google.maps.event.addDomListener(window, 'load', function () {
+            var places = new google.maps.places.Autocomplete(document.getElementById('location'));
+            google.maps.event.addListener(places, 'place_changed', function () {
+                var place = places.getPlace();
+                var address = place.formatted_address;
+                // var latitude = place.geometry.location.A;
+                // var longitude = place.geometry.location.F;
+                // var mesg = "Address: " + address;
+                // mesg += "\nLatitude: " + latitude;
+                // mesg += "\nLongitude: " + longitude;
+                // alert(mesg);
+            });
+        });
